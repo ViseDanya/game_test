cb30f76535902efeaed0ae36765ea51521599b41
Daniel Konviser <djkonviser@gmail.com>
2025-01-05 19:56:49 -0500

    cleanup cmake, add enet

---
+++ src/client.cpp
@@ -0,0 +1,67 @@
+#include <enet/enet.h>
+#include <iostream>
+
+int main (int argc, char ** argv) 
+{
+    if (enet_initialize () != 0)
+    {
+        std::cerr << "An error occurred while initializing ENet." << std::endl;
+        return EXIT_FAILURE;
+    }
+    atexit (enet_deinitialize);
+
+ENetHost * client;
+ 
+client = enet_host_create (NULL /* create a client host */,
+            1 /* only allow 1 outgoing connection */,
+            2 /* allow up 2 channels to be used, 0 and 1 */,
+            0 /* assume any amount of incoming bandwidth */,
+            0 /* assume any amount of outgoing bandwidth */);
+ 
+if (client == NULL)
+{
+    fprintf (stderr, 
+             "An error occurred while trying to create an ENet client host.\n");
+    exit (EXIT_FAILURE);
+}
+
+ENetEvent event;
+ 
+/* Wait up to 1000 milliseconds for an event. */
+while (enet_host_service (client, & event, 1000) > 0)
+{
+    switch (event.type)
+    {
+    case ENET_EVENT_TYPE_CONNECT:
+        printf ("A new client connected from %x:%u.\n", 
+                event.peer -> address.host,
+                event.peer -> address.port);
+ 
+        /* Store any relevant client information here. */
+        // event.peer -> data = "Client information";
+ 
+        break;
+ 
+    case ENET_EVENT_TYPE_RECEIVE:
+        printf ("A packet of length %u containing %s was received from %s on channel %u.\n",
+                event.packet -> dataLength,
+                event.packet -> data,
+                event.peer -> data,
+                event.channelID);
+ 
+        /* Clean up the packet now that we're done using it. */
+        enet_packet_destroy (event.packet);
+        
+        break;
+       
+    case ENET_EVENT_TYPE_DISCONNECT:
+        printf ("%s disconnected.\n", event.peer -> data);
+ 
+        /* Reset the peer's client information. */
+ 
+        event.peer -> data = NULL;
+    }
+}
+
+enet_host_destroy(client);
+}
