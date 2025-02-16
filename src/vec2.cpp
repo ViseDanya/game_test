9d3433e0cd558ddb39107b1d145ed01dbf39c4aa
Daniel Konviser <djkonviser@gmail.com>
2025-02-15 22:05:36 -0500

    remove vec2 source files

--- src/vec2.cpp
+++
@@ -1,7 +0,0 @@
-#include "vec2.h"
-
-const vec2 vec2::left = vec2(-1,0);
-const vec2 vec2::right = vec2(1,0);
-const vec2 vec2::up = vec2(0,1);
-const vec2 vec2::down = vec2(0,-1);
-const vec2 vec2::zero = vec2(0,0);
